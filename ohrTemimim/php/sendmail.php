fd2fbe5c327f8de505b0040cc22423396dfe81a9
Avi Tapp <andrew.tapp@gmail.com>
2014-02-07 12:12:12 -0500

    rewrote wrap order form php file

--- ohrTemimim/php/sendmail.php
+++ ohrTemimim/php/sendmail.php
@@ -1,5 +1,14 @@
 <?php
-	$email = $_REQUEST['email'] ;
-	$message = $_REQUEST['message'] ;
-	mail( "andrew.tapp@gmail.com", "Feedback Form Results",    $message, "From: $email" );
+	$name = $_REQUEST['name'];
+	$wrapInside = $_REQUEST['wrapInside'];
+	$wrapOutside = $_REQUEST['wrapOutside'];
+	$howMany = $_REQUEST['howMany'];
+	$drink = $_REQUEST['drink'];
+	$details = $_REQUEST['details'];
+	$balance = $_REQUEST['balance'];
+	$payment = $_REQUEST['payment'];
+	$contact = $_REQUEST['contact'];
+	$message = "You have received a new Wednesday Wrap order. ".$name." would like ".$howMany." wraps, ".$wrapInside." style using a ".$wrapOutside." wrap. If thirsty, ".$name." wants ".$drink.". ".$name." owes ".$balance." and wants to pay with ".$payment.". If you have any questions, you can email or call ".$name." at ".$contact.". ".$details;
+
+	mail( "andrew.tapp@gmail.com", "WWW.OrderForm.com", $message, "From: HPCatering@gmail.com" );
 	?>
